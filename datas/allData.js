c73fe268d2ddbb6e3652ddaeccf2b2351e662a4e
Sanskar Piya <sanspiya915@gmail.com>
2026-04-10 13:17:11 +0545

    Add: adding all songs data into array of objects

---
+++ datas/allData.js
@@ -0,0 +1,88 @@
+const popularArtists = [
+  { id: 1, image: "popular_artist1.jpg", name: "Arijit Singh", role: "Artist" },
+  { id: 2, image: "popular_artist2.jpg", name: "John Rai", role: "Artist" },
+  { id: 3, image: "popular_artist3.jpg", name: "The Elements", role: "Artist" },
+  { id: 4, image: "popular_artist4.jpg", name: "Tribal Rain", role: "Artist" },
+  { id: 5, image: "popular_artist5.jpg", name: "Oasis Thapa", role: "Artist" },
+  { id: 6, image: "popular_artist6.jpg", name: "Kuma Sagar", role: "Artist" },
+];
+
+const recentlyPlayed = [
+  {
+    id: 1,
+    image: "card1img.jpeg",
+    name: "Top 50-Global",
+    description: "Your daily update of recent played tracks...",
+  },
+  {
+    id: 2,
+    image: "card2img.jpeg",
+    name: "Mahiye Jinna Sohna",
+    description: "Darshan Raval",
+  },
+  {
+    id: 3,
+    image: "card3img.jpeg",
+    name: "Mere Pass Tum Ho",
+    description: "Rahat Fateh Ali Khan",
+  },
+  {
+    id: 4,
+    image: "card4img.jpeg",
+    name: "Naa Ready",
+    description: "Anirudh Ravichander Thapalathy Vijay, Asal",
+  },
+  {
+    id: 5,
+    image: "card5img.jpeg",
+    name: "Top 50-Global",
+    description: "Your weekly update of recent played tracks..",
+  },
+  {
+    id: 6,
+    image: "card6img.jpeg",
+    name: "Top 50-India",
+    description: "Your Weekly update of recent played tracks..",
+  },
+];
+
+const trendingSongs = [
+  {
+    id: 1,
+    image: "trending_song1.jpg",
+    song_name: "Najeek",
+    singer_name: "Bartika Eam Rai",
+  },
+  {
+    id: 2,
+    image: "trending_song2.jpg",
+    song_name: "I Wanna Be Yours",
+    singer_name: "Arctic Monkeys",
+  },
+  {
+    id: 3,
+    image: "trending_song3.jpg",
+    song_name: "GO",
+    singer_name: "BLACKPINK",
+  },
+  {
+    id: 4,
+    image: "trending_song4.jpg",
+    song_name: "Kasari",
+    singer_name: "Yabesh Thapa",
+  },
+  {
+    id: 5,
+    image: "trending_song5.jpg",
+    song_name: "Khat",
+    singer_name: "Navjot Ahuja",
+  },
+  {
+    id: 6,
+    image: "trending_song6.jpg",
+    song_name: "Ko Cha Ra (Official Audio)",
+    singer_name: "John Rai",
+  },
+];
+
+module.exports = { popularArtists, recentlyPlayed, trendingSongs };
